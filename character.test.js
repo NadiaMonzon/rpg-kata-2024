b75293d0742a4ae569eb639ae41899b168a2d8e1
NadiaMonzon <nadiamonque99@gmail.com>
2024-11-08 19:39:39 +0000

    Red: add test for healing another character

--- character.test.js
+++ character.test.js
@@ -6,30 +6,45 @@ describe("basic pj", () => {
     const character = new Character();
     expect(character.health).toBe(1000);
   });
+
   it("should be level 1 when the character is created", () => {
     const character = new Character();
     expect(character.level).toBe(1);
   });
+
   it("should be alive when the character is created", () => {
     const character = new Character();
     expect(character.alive).toBeTruthy();
   });
+
   it("character should be able to attack", () => {
     const character = new Character();
     const characterTwo = new Character();
     character.pjAttack(800, characterTwo);
-    expect(characterTwo.health).toBe(200);   
-  } )
+    expect(characterTwo.health).toBe(200);
+  });
+
   it(" When damage received exceeds current Health, Health becomes 0", () => {
     const character = new Character();
     const characterTwo = new Character();
     character.pjAttack(1500, characterTwo);
     expect(characterTwo.health).toBe(0);
-  })
+  });
+
   it("When the pj health is equal to 0, the pj is dead", () => {
     const character = new Character();
     const characterTwo = new Character();
     character.pjAttack(1500, characterTwo);
     expect(characterTwo.alive).toBeFalsy();
-  })
+  });
+
+  it("A character can heal a character", () => {
+    const character = new Character();
+    const characterTwo = new Character();
+
+    character.pjAttack(800, characterTwo);
+    character.pjHeal(200, characterTwo);
+
+    expect(characterTwo.health).toBe(400);
+  });
 });
